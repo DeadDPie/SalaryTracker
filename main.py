a0c27f3b59271ca15f2b14ec5c22d75089774b0b
DeadDPie <elizabethfox434@gmail.com>
2023-06-09 16:12:09 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI
+from api import router
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(title="SalaryTracker")
+origins = ["*"] #позволили всем сайтам обращаться к бэку
+app.include_router(router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],#все запросы
+    allow_headers=["*"],
+)
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
+
+
