8919c50b49cd66947d0cdf209fd739af5210e070
DeadDPie <elizabethfox434@gmail.com>
2023-06-14 23:08:07 +0700

    сделала простенькие тесты

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,13 +1,40 @@
-
 from fastapi.testclient import TestClient
-
+from .variables import Vars as var
 from main import app
-
-
 
 client = TestClient(app)
 
-
 def test_get_users():
+    """
+    Получение сотрудников
+    """
     response = client.get("/api/users")
     assert response.status_code == 200
+
+def test_not_auth_get_salary():
+    """
+        Получение информации о зарплате неавторизованным работником
+    """
+    response = client.get("/api/salary")
+    assert response.status_code == 401
+
+def test_auth_create_token():
+    """
+        Создание токена для авторизованного работника
+    """
+    response = client.post("/api/token", data={"username":var.username,"password":var.password})
+    assert response.status_code == 200
+
+def test_auth_wrong_create_token():
+    """
+        Создание токена для работника с некорректным логином
+    """
+    response = client.post("/api/token", data={"username":"wrong","password":var.password})
+    assert response.status_code == 401
+def test_not_auth_create_token():
+    """
+       Создание токена для неавторизованного работника
+    """
+    response = client.post("/api/token",)
+    assert response.status_code == 422
+
