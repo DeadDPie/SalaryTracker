2bf50f8899649479599edff400870e8bf5cd4d30
DeadDPie <114298893+DeadDPie@users.noreply.github.com>
2023-06-19 14:45:54 +0700

    изменила путь для линукса создала докер файл пустой

--- database/database.py
+++ database/database.py
@@ -5,5 +5,5 @@ import os
 class DB:
     cwd = os.getcwd()
     cwd = cwd[:cwd.index("SalaryTracker")+13]
-    with open(cwd+"\\database\\fake_db.json") as f:
+    with open(cwd+"/database/fake_db.json") as f:
         data = json.load(f)
